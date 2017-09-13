9d0d877a46838d1421ab8c7ffe17737e2c8dafc7
Mark Nelson <mark.x.nelson@oracle.com>
2017-09-13 11:47:45 -0400

    run DDLs

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,11 +22,39 @@ trap _kill SIGKILL
 echo "Waiting for database to be available..."
 while ! nc database 1521 < /dev/null; do sleep 9; done
 # sleep for some extra time for db setup
-sleep 30
+sleep 80
+
+# if there are any ddl files, run them 
+if [ -e /u01/oracle/create-users.sql ];
+then 
+   echo "Creating users..."
+   java -classpath /u01/oracle/wlserver/server/lib/weblogic.jar \
+   utils.Schema \
+   jdbc:oracle:thin:@//database:1521/OraDoc.my.domain.com \
+   oracle.jdbc.OracleDriver \
+   -u "sys as sysdba" \
+   -p Welcome1 \
+   /u01/oracle/create-users.sql
+fi
+
+# userid hard coded for now - fix this later
+if [ -e /u01/oracle/create-schema.sql ];
+then 
+   echo "Creating schema..." 
+   java -classpath /u01/oracle/wlserver/server/lib/weblogic.jar \
+   utils.Schema \
+   jdbc:oracle:thin:@//database:1521/OraDoc.my.domain.com \
+   oracle.jdbc.OracleDriver \
+   -u c##mark \
+   -p Welcome1 \
+   /u01/oracle/create-schema.sql
+fi
+
 
 DOMAIN_HOME=/u01/oracle/domains/base_domain 
 
 # Start Admin Server and tail the logs
+echo "Starting WebLogic..."
 ${DOMAIN_HOME}/startWebLogic.sh &
 childPID=$!
 wait $childPID
